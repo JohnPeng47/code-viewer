c54607efd5795dd705799213b88a6cc0054b3bfe
JohnPeng47 <johnpeng47@gmail.com>
2025-11-30 22:52:47 -0500

    package.json naming fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-export { CodeViewer as CodeSnapshotWidget } from './components/CodeViewer/ViewerContainer';
-export type { CodeViewerProps as CodeSnapshotWidgetProps } from './components/CodeViewer/ViewerContainer';
+// Main component
+export { CodeViewer as CodeSnapshotWidget } from "./components/CodeViewer/ViewerContainer";
+export type { CodeViewerProps as CodeSnapshotWidgetProps } from "./components/CodeViewer/ViewerContainer";
 
-export * from './types';
-export { contentCache } from './services/contentService';
-export { useFileContent } from './hooks/useFileContent';
-export { useSnapshotNavigation } from './hooks/useSnapshotNavigation';
+// Types
+export type { CodeSnapshot, GitHubFileInfo } from "./types";
